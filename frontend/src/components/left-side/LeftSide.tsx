761de2978d2d0b469cf760e225430c5c255c2a45
Justus Hansen <justus.hansen@psil.fi>
2022-04-25 19:07:34 +0300

    Add content to left side

--- frontend/src/components/left-side/LeftSide.tsx
+++ frontend/src/components/left-side/LeftSide.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Box from "../util/Box";
+import Logo from "../../img/pvlspeedrunslogo.png"
 
 const BoxContainer = styled.div`
   width: 300px;
@@ -7,11 +8,69 @@ const BoxContainer = styled.div`
   margin: 50px;
 `;
 
+const LogoImg = styled.img`
+  align-self: center;
+`;
+
+const LogoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10%;
+`;
+
+const TextContainer = styled.div`
+  padding-left: 10%;
+  overflow: auto;
+`;
+
+const Title = styled.h1`
+  font-size: var(--small-text);
+  margin-bottom: 0;
+  margin-top: 10%;
+`;
+
+const Link = styled.a`
+  font-size: var(--very-small-text);
+`;
+
+const NameList = styled.ul`
+  margin-top: 5%;
+`;
+
+const Name = styled.li`
+  font-size: var(--very-small-text);
+`;
+
+const Description = styled.p`
+  font-size: var(--very-small-text);
+`;
+
 const LeftSide = () => {
   return (
     <BoxContainer>
       <Box>
-        <p>PVL-speedruns</p>
+        <>
+          <LogoContainer>
+            <LogoImg src={Logo} width={"80%"}/>
+          </LogoContainer>
+          <TextContainer>
+            <Title>Päivölä:</Title>
+            <Link href="https://paivola.fi/">https://paivola.fi/</Link>
+            <Title>PSIL:</Title>
+            <Link href="https://psil.fi/">https://psil.fi/</Link>
+            <Title>Tekijät:</Title>
+            <NameList>
+              <Name>Joonatan (Zetat)</Name>
+              <Name>Zendium (Zetat)</Name>
+              <Name>Motsgar (Zetat)</Name>
+            </NameList>
+            <Description>
+              Tämä sivusto luotiin 25.4 2022 PSILin aikana täysin luvallisesti.
+            </Description>
+          </TextContainer>
+        </>
       </Box>
     </BoxContainer>
   );
